b6497bbf788a8599df84110f4f002d1513a7d858
dyrnq <dyrnq@qq.com>
2023-12-30 00:15:10 +0800

    Add springdoc configuration

--- backend/api/src/main/java/net/mhgoi/blog/config/Swagger3Config.java
+++ backend/api/src/main/java/net/mhgoi/blog/config/Swagger3Config.java
@@ -5,9 +5,6 @@ import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,17 +15,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Swagger3Config {
 
-//
-//    @Bean
-//    public OpenAPI springShopOpenAPI() {
-//        return new OpenAPI()
-//                .info(new Info().title("SpringShop API")
-//                        .description("Spring shop sample application")
-//                        .version("v0.0.1")
-//                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-//                .externalDocs(new ExternalDocumentation()
-//                        .description("SpringShop Wiki Documentation")
-//                        .url("https://springshop.wiki.github.org/docs"));
-//    }
+    @Bean
+    public OpenAPI mhgoiOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("唔该博客")
+                        .description("blog.mhgoi.net")
+                        .version("1.0.0")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("mhgoi Wiki Documentation")
+                        .url("https://doc.mhgoi.net"));
+    }
 
 }
